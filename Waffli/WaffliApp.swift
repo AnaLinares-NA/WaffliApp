696803438720367270452dd1a880bf89ce0c7159
Ana Paola Linares Guzman <ana.linares@comunidad.unam.mx>
2026-05-23 13:24:33 -0600

    Initial Commit

---
+++ Waffli/WaffliApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaffliApp.swift
+//  Waffli
+//
+//  Created by Ana Linares Guzmán on 23/05/26.
+//
+
+import SwiftUI
+
+@main
+struct WaffliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
